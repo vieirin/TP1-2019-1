194402741015751c0c26f71757d6201670544bf1
Manoel Vieira <vieiranetoc@gmail.com>
2019-03-23 17:34:43 -0300

    Tests working

--- src/domains/EventCode.cpp
+++ src/domains/EventCode.cpp
@@ -3,8 +3,9 @@
 EventCode::EventCode(std::string code)
 {
     std::regex code_regex("([0-9])\\w{2}");
+    std::smatch match;
 
-    if (code.length() > 0 && code.length() < 4 && std::regex_match(code, code_regex)) {
+    if (std::regex_match(code, match, code_regex)) {
         this->code = code;
     } else {
         throw std::invalid_argument("Event code must be like XXX");
--- src/domains/tests/EventCode_test.cpp
+++ src/domains/tests/EventCode_test.cpp
@@ -3,8 +3,20 @@
 #include <stdexcept>
 #include <string>
 
-TEST(EventCodeTest, OneDigit) {
-  ASSERT_THROW(EventCode("abc"), std::invalid_argument);
+TEST(EventCodeTest, OneDigitFail) {
+  ASSERT_THROW(EventCode("1"), std::invalid_argument);
+}
+
+TEST(EventCodeTest, TwoDigitsFail) {
+  ASSERT_THROW(EventCode("10"), std::invalid_argument);
+}
+
+TEST(EventCodeTest, ThreeDigitsPass) {
+  ASSERT_THROW(EventCode("100"), std::invalid_argument);
+}
+
+TEST(EventCodeTest, FourDigitsFail) {
+  ASSERT_THROW(EventCode("1001"), std::invalid_argument);
 }
 
 int main(int argc, char **argv) {
