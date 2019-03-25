92b47d8810f681680b68f6b93c2dcd5e5d7511ce
Vieira Neto <vieiranetoc@gmail.com>
2019-03-25 10:58:45 -0300

    Adds eventType enum

---
+++ include/domains/enums/eventType.hpp
@@ -0,0 +1,7 @@
+enum eventType
+{
+    TEATRO,
+    ESPORTE,
+    NACIONAL,
+    INTERNACIONAL
+}
--- include/domains/enums/states.hpp
+++ include/domains/enums/states.hpp
@@ -1,10 +1,31 @@
 
-enum State {
-    AC, AL, AP, AM,
-    BA, CE, DF, ES,
-    GO, MA, MT, MS,
-    MG, PA, PB, PR,
-    PE, PI, RJ, RN,
-    RS, RO, RR, SC,
-    SP, SE, TO
+enum state
+{
+    AC,
+    AL,
+    AP,
+    AM,
+    BA,
+    CE,
+    DF,
+    ES,
+    GO,
+    MA,
+    MT,
+    MS,
+    MG,
+    PA,
+    PB,
+    PR,
+    PE,
+    PI,
+    RJ,
+    RN,
+    RS,
+    RO,
+    RR,
+    SC,
+    SP,
+    SE,
+    TO
 }
