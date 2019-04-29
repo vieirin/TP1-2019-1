68bd4d5347de05b8fcf8eac56a2985084efe387d
Vieira Neto <vieiranetoc@gmail.com>
2019-04-29 03:44:13 +0000

    Fix some compiling errors

--- src/domains/EventType.cpp
+++ src/domains/EventType.cpp
@@ -1,10 +1,9 @@
 #include <domains/EventType.hpp>
 
 EventType::EventType(std::string typeName) {
-    evType =
-        std::unordered_map<std::string, std::int>{{"TEATRO", 1},
+    evType = std::unordered_map<std::string, int>{{"TEATRO", 1},
                                                   {"ESPORTE", 2},
                                                   {"SHOW NACIONAL", 3},
                                                   {"SHOW INTERNACIONAL", 4}};
-    type = evType[typename];
+    type   = evType[typeName];
 }
