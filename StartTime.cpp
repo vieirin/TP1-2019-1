d8e235a11b77a6aa19f10bc1d168461a89329927
GuiCo3lho <37370101+GuiCo3lho@users.noreply.github.com>
2019-04-30 00:59:46 -0300

    Add files via upload

---
+++ StartTime.cpp
@@ -0,0 +1,14 @@
+#include <cctype>
+#include <domains/StartTime.hpp>
+
+StartTime::StartTime(std::string time) { setStartTime(time); }
+void StartTime::setStartTime(std::string time) {
+    std::regex regex_time("^[012]{1}[0-9]{1}:[134]{1}[05]{1}");
+    std::smatch match;
+    if (std::regex_match(time, match, regex_time)) {
+        this->time;
+    } else {
+        throw std::invalid_argument(
+            "Time has to be of type HH:MM following the 24h rule");
+    }
+}
