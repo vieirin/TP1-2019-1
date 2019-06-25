27f2411b197788cad17b830006699c20ba9275df
guico3lho <gui_minervino@hotmail.com>
2019-06-25 01:17:55 -0300

    Correcting User class

--- include/entities/User.hpp
+++ include/entities/User.hpp
@@ -10,10 +10,12 @@ class User {
   private:
     CPF *cpf;
     Password *passwd;
+    std::shared_ptr<CreditCard> credit_card;
 
   public:
     /** Presentation Constructor takes 2 parameter*/
-    User(std::string cpfNumber, std::string pwd);
+    User(std::string cpfNumber, std::string pwd,
+         std::shared_ptr<CreditCard> credit_card);
     ~User() {}
     bool checkPassword(std::string password);
     std::string getCPFNumber();
--- src/entities/User.cpp
+++ src/entities/User.cpp
@@ -1,9 +1,11 @@
 #include <domains/CPF.hpp>
 #include <entities/User.hpp>
 
-User::User(std::string cpfNumber, std::string pwd) {
-    this->cpf    = new CPF(cpfNumber);
-    this->passwd = new Password(pwd);
+User::User(std::string cpfNumber, std::string pwd,
+           std::shared_ptr<CreditCard> credit_card) {
+    this->cpf         = new CPF(cpfNumber);
+    this->passwd      = new Password(pwd);
+    this->credit_card = credit_card;
 }
 
 bool User::checkPassword(std::string password) {
